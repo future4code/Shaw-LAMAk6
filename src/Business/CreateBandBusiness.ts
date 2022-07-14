929739db31fb53ac9b2971857243b0d9ea3404ff
Pablo Andrade de Lima Silva <98933578+PbLRJ@users.noreply.github.com>
2022-07-14 16:24:44 -0300

    Endpoint de criar banda

---
+++ src/Business/CreateBandBusiness.ts
@@ -0,0 +1,44 @@
+import BandCreatedData from '../Data/userData'
+import { CreateBand, CreateBandUser } from '../Models/CreateBand'
+import { IdGenerate } from '../Services/idGenerate'
+
+
+
+export default class CreateBandBusiness {
+    constructor(
+        private userData: BandCreatedData,
+        private idGenerator: IdGenerate        
+    ) { }
+
+    createBand = async (input: CreateBand) => {
+
+        const { name, music_genre, responsible } = input
+
+        if (!name || !music_genre || !responsible) {
+            throw new Error('Campos inválidos')
+        }
+
+        const bandExists = await this.userData.findByBand(name)
+        if (bandExists) {
+            throw new Error('Banda já cadastrada !')
+        }
+
+        const id = this.idGenerator.generateId()
+
+        const bandCreate = new CreateBandUser(
+            id,
+            name,
+            music_genre,
+            responsible
+        )
+
+        await this.userData.insert(bandCreate)
+
+
+    }
+
+    
+
+    
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-console.log("to rodando");
+import CreateBandBusiness from "./Business/CreateBandBusiness";
+import { CreateBandController } from "./Controller/CreateBandController";
+import BandCreatedData from "./Data/userData";
+import app from "./Services/app";
+import { IdGenerate } from "./Services/idGenerate";
 
-//roda carai
+const bandController = new CreateBandController(
+    new CreateBandBusiness(
+        new BandCreatedData(),
+        new IdGenerate(),
+    )
+)
+
+
+app.post('/band', bandController.createBand )
