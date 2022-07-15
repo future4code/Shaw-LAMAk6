a1f624b5ce1fe4a8aff764372b2ce4e348d90d78
Natã Lucas Silva <99276733+NatanBreaks@users.noreply.github.com>
2022-07-15 13:49:35 -0300

    aa

--- src/Services/HashManager.ts
+++ src/Services/HashManager.ts
@@ -1,13 +1,14 @@
 import * as bcrypt from 'bcryptjs';
 
-export class HashManager{
+
+export class HashManager {
   hash = async (plainText: string) => {
     const rounds = Number(process.env.BCRYPT_COST);
     const salt = await bcrypt.genSalt(rounds);
-    return bcrypt.hash(plainText, salt)
-  }
+    return bcrypt.hash(plainText, salt);
+  };
 
   compare = (plainText: string, cypherText: string): Promise<boolean> => {
-    return bcrypt.compare(plainText, cypherText)
-  }
+    return bcrypt.compare(plainText, cypherText);
+  };
 }
